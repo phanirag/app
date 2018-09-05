5ce8d85507bb2f555ec13bb53b5867503738d2fb
raghav <raghav@Raghav>
2018-09-05 22:49:27 +0530

    WHM - First Commit and push

---
+++ src/com/app/controller/ItemController.java
@@ -0,0 +1,79 @@
+package com.app.controller;
+
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.app.model.Item;
+import com.app.service.IItemService;
+
+@Controller
+public class ItemController {
+
+	@Autowired
+	private IItemService service;
+
+	@Autowired
+	private ServletContext context;
+	
+	@RequestMapping("/regItem")
+	public String showPage(){
+		return "ItemReg";
+	}
+	
+	@RequestMapping(value="/insertItem",method=RequestMethod.POST)
+	public String saveItem(@ModelAttribute("item")Item Item,ModelMap map){
+		int Itemid=service.saveItem(Item);
+		String message="Record id"+Itemid;
+		map.addAttribute("msg",message);
+		return "ItemReg";
+	}
+	
+	@RequestMapping(value="/viewAllItem")
+	public String showallItem(ModelMap map){
+		List<Item> Itemlist=service.getallItem();
+		map.addAttribute("list",Itemlist);
+		return "ItemData";
+	}
+	@RequestMapping(value="/updateID")
+	public String showupdateid(@RequestParam("itemId")int id,ModelMap map){
+		Item Item=service.getItembyid(id);
+		map.addAttribute("Itemobj",Item);
+		return "ItemUpdate";
+	}
+	
+	@RequestMapping(value="/updateItem",method=RequestMethod.POST)
+	public String updateItem(@ModelAttribute("Item")Item Item){
+		service.updateItem(Item);
+		return "redirect:viewAllItem";
+	}
+	
+	@RequestMapping(value="/deleteItem")
+	public String deleteItem(@RequestParam("itemId")int id){
+		service.deleteItem(id);
+		return "redirect:viewAllItem";
+	}
+	
+	@RequestMapping(value="/Itemexeclexport")
+	public String showExcelData(ModelMap map){
+		List<Item> Item=service.getallItem();
+		map.addAttribute("Item",Item);
+		return "ItemExcelView";
+	}
+	
+	@RequestMapping(value="/Itempdfexport")
+	public String showPdfData(ModelMap map){
+		List<Item> Item=service.getallItem();
+		map.addAttribute("Item",Item);
+		return "ItemPdfView";
+	}
+
+}
---
+++ src/com/app/filter/SessionCheckFilter.java
@@ -0,0 +1,70 @@
+package com.app.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SessionCheckFilter implements Filter{
+
+	List<String> uris=null;
+
+	@Override
+	public void init(FilterConfig cfg) throws ServletException {
+		
+		String s=cfg.getInitParameter("NoCheckUris");
+		StringTokenizer st=new StringTokenizer(s,",");
+		
+		uris=new ArrayList<String>();
+		
+		while (st.hasMoreElements()) {
+			String sf=st.nextToken();
+			uris.add(sf);
+		}
+				
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain fc) throws IOException, ServletException {
+		HttpServletRequest hreq=(HttpServletRequest)req;
+		HttpServletResponse hres=(HttpServletResponse)res;
+		
+		/*cache clear on logout*/
+		hres.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); 
+		hres.setHeader("Pragma", "no-cache");	
+		hres.setDateHeader("Expires", 0);
+		
+		String uri=hreq.getRequestURI();
+		
+		boolean b=uris.contains(uri);
+		
+		try {
+			if(!b){
+				HttpSession ses=hreq.getSession(false);
+				if(ses==null || ses.getAttribute("userName")==null){
+					hres.sendRedirect(hreq.getContextPath()+"/mvc/showLogin");
+				}
+			}
+		} catch (Exception e) {
+		}
+		fc.doFilter(req, res);
+		
+	}
+
+	@Override
+	public void destroy() {
+		uris=null;
+	}
+
+}
